e42e6c161d0b1222250d8782fcf208f508387c1c
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-07-28 14:48:16 -0300

    chore: add sync script

---
+++ scripts/syncPublic.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd ../api-mocking-app-sharing
+
+cp -r ../tcc-app-sharing .
+rm -rf tcc-app-sharing/.git
+cp -r tcc-app-sharing/* .
+rm -rf tcc-app-sharing scripts
+
+git add .
+git commit --amend --no-edit
