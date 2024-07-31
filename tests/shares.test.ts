ded9eae9593f9f98ff19651646ea34de56645e8c
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-07-31 20:58:09 -0300

    chore: remove test descriptions

--- tests/shares.test.ts
+++ tests/shares.test.ts
@@ -24,9 +24,7 @@ describe('Shares', () => {
     await app.close();
   });
 
-  /**
-   * Exemplo (para habilitar, remova o `.skip`)
-   */
+  // Exemplo (para habilitar, remova o `.skip`)
   test.skip('example', async () => {
     const response = await supertest(app.server)
       .post('/shares/files')
@@ -38,34 +36,18 @@ describe('Shares', () => {
     expect(response.status).toBe(200);
   });
 
-  /**
-   * Teste 1: Deve retornar uma resposta de sucesso quando um arquivo foi
-   * compartilhado sem conversão.
-   */
   test('case 1', async () => {
     // Implemente aqui...
   });
 
-  /**
-   * Teste 2: Deve retornar uma resposta de sucesso quando um arquivo foi
-   * compartilhado com conversão.
-   */
   test('case 2', async () => {
     // Implemente aqui...
   });
 
-  /**
-   * Teste 3: Deve retornar uma resposta de erro quando a conversão de um
-   * arquivo compartilhado resultou em um erro.
-   */
   test('case 3', async () => {
     // Implemente aqui...
   });
 
-  /**
-   * Teste 4: Deve retornar uma resposta de erro quando não foi possível
-   * utilizar a API de conversão por um erro desconhecido.
-   */
   test('case 4', async () => {
     // Implemente aqui...
   });
