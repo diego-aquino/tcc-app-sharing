58ac8c403a402691ef2c33560af65216393888ef
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-08-05 13:51:57 -0300

    feat: sync branches automatically

--- scripts/sync-public.sh
+++ scripts/sync-public.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+git checkout main-v1
+git rebase main
+git push --force
+
+git checkout main-v2
+git rebase main-v1
+git push --force
+
+git checkout main
+
 cd ../api-mocking-app-sharing
 
 rsync ../tcc-app-sharing/ . \
